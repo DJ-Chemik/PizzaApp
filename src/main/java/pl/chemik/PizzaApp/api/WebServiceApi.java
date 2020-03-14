2eda56dc9b03cf30b65f19b51a23bfc428e4a14f
DJ Chemik <dj-chemik@wp.pl>
2020-03-14 23:36:56 +0100

    Add GET method to access by web

--- src/main/java/pl/chemik/PizzaApp/api/WebServiceApi.java
+++ src/main/java/pl/chemik/PizzaApp/api/WebServiceApi.java
@@ -2,6 +2,7 @@ package pl.chemik.PizzaApp.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.chemik.PizzaApp.objects.ingredients.vegetables.Broccoli;
 import pl.chemik.PizzaApp.objects.managers.MarioPizzaPlaceManager;
@@ -26,9 +27,30 @@ public class WebServiceApi {
         return "Hello in web app with the best pizzas :)";
     }
 
-    @GetMapping("/pizza")
-    public List<Pizza> getNameOfComponent(){
+    @GetMapping("/pizza/all")
+    public List<Pizza> getPizzas(){
         return pizzzas;
+    }
+
+    @GetMapping("/pizza")
+    public List<String> getListOfNamesAndNumberPizzas(){
+        List<String> names = new ArrayList<>();
+        pizzzas.forEach(pizza -> {
+            names.add(pizza.getNumberInMenu() + ". " + pizza.getName());
+        });
+        return names;
+    }
+
+    @GetMapping("/pizza/names")
+    public List<String> getNameOfPizzas(){
+        List<String> names = new ArrayList<>();
+        pizzzas.forEach(pizza -> {names.add(pizza.getName());});
+        return names;
+    }
+
+    @GetMapping("/pizza/{id}")
+    public Pizza getOnePizzaById(@PathVariable("id") int id){
+        return pizzzas.stream().filter(pizza -> pizza.getNumberInMenu()==(id)).findFirst().get();
     }
 
 }
